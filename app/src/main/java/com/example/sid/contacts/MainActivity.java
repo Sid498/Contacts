7fb1541ba53cd0bdcb5576f9189cc7b52a69207e
Sid498 <sidpawar619@gmail.com>
2017-05-13 23:12:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sid/contacts/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sid.contacts;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_CONTACTS = 1234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OpenContacts(View v){
+        Button btn = (Button) findViewById(R.id.btnContacts);
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivity(intent);
+    }
+}
